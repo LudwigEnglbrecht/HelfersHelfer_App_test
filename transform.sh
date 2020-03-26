76c7e8cc1dd7f471caaef6f8496238f203ae4486
Ludwig.Englbrecht <113e398a4a91902bcb8b6399b27f7a70@gmx.de>
2020-03-26 22:46:22 +0100

    Create transform.sh

---
+++ transform.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# remove unnecessary things
+cd HelfersHelfer
+rm -f README.md
+rm -rf IntensivStationsParser
+cd ..
+
+# List all files containing "window."-function window does not exist in NativeScript, therefore ensure to remove any explicit dependencies on the browser's global window object in your code.
+grep -iRl "window." ./
