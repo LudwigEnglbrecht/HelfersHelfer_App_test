28c5254995b437d54984132148020c5413f5de39
Ludwig.Englbrecht <113e398a4a91902bcb8b6399b27f7a70@gmx.de>
2020-03-26 23:19:15 +0100

    Update transform.sh

--- transform.sh
+++ transform.sh
@@ -8,3 +8,6 @@ cd ..
 
 # List all files containing "window."-function window does not exist in NativeScript, therefore ensure to remove any explicit dependencies on the browser's global window object in your code.
 grep -iRl "window." ./
+
+
+sed -i 's/RouterModule/NativeScriptRouterModule/g' ./src/app/app-routing.module.ts
